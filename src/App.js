2f2ce7cda245ab998101e63f747d68dcc9ffe37b
Dovran <dovranrustemov@gmail.com>
2022-11-07 12:38:57 -0500

    Added todos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { useState } from 'react'
 
 function App() {
   const [todos, setTodos] = useState('')
-  return <div>App</div>
+
+  return (
+    <header>
+      <h1>App</h1>
+      <input type='text' placeholder='Todo' />
+    </header>
+  )
 }
 
 export default App
