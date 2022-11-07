5cfded47b61eba090b145525a0fd0dfeb180189e
Dovran <dovranrustemov@gmail.com>
2022-11-07 12:43:33 -0500

    Added todo button

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import React, { useState } from 'react'
 function App() {
   const [todos, setTodos] = useState('')
 
+  const addTodo = () => {
+    console.log('Add')
+  }
+
+  const changeTodo = () => {
+    console.log('Input Change')
+  }
+
   return (
     <header>
       <h1>App</h1>
-      <input type='text' placeholder='Todo' />
+      <input onChange={changeTodo} type='text' placeholder='Todo' />
+      <button onClick={addTodo}>Add</button>
     </header>
   )
 }
