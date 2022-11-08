f5bc5d064a95a09eed9ceca5093689240bfe6399
Dovran <dovranrustemov@gmail.com>
2022-11-08 13:55:25 -0500

    Added e.preventDefault

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 
 function App() {
   const [todos, setTodos] = useState('')
+  const [inputValue, setInputValue] = useState('')
 
-  const addTodo = () => {
+  const addTodo = (e) => {
     console.log('Add')
+    e.preventDefault()
   }
 
   const changeTodo = () => {
@@ -14,7 +16,12 @@ function App() {
   return (
     <header>
       <h1>App</h1>
-      <input onChange={changeTodo} type='text' placeholder='Todo' />
+      <input
+        onChange={changeTodo}
+        type='text'
+        placeholder='Todo'
+        imputValue={todos}
+      />
       <button onClick={addTodo}>Add</button>
     </header>
   )
