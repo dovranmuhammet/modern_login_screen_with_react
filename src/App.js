d121da1bd29524284f90c5a8fdce16c5f93a94f3
Dovran <dovranrustemov@gmail.com>
2022-11-11 16:23:17 -0500

    Added some html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react'
+import { data } from './data'
 
 export default function App() {
   const [count, setCount] = useState(0)
 
-  return <h1>Counter: {count}</h1>
+  const plusCount = () => {
+    setCount(count + 1)
+  }
+
+  const minusCount = () => {
+    setCount(count - 1)
+  }
+
+  return (
+    <div className='app'>
+      <h1>Counter: {count}</h1>
+      <button onClick={plusCount}>Increase</button>
+      <button onClick={minusCount}>Decrease</button>
+      <h1>Todo List</h1>
+      <button>Add</button>
+    </div>
+  )
 }
---
+++ src/data.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    name: 'H',
+    number: 1,
+    lastName: 'Ch',
+  },
+  {
+    name: 'P',
+    number: 2,
+    lastName: 'Kf',
+  },
+  {
+    name: 'L',
+    number: 3,
+    lastName: 'D',
+  },
+]
